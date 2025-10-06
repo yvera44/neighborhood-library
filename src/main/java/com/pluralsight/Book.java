daf0fb63f01bf579418c32afca88a82d5967558c
YairVera <yairvera44@gmail.com>
2025-10-06 15:19:00 -0500

    set up book class

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,4 +1,42 @@
 package com.pluralsight;
 
 public class Book {
+
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
+
+    public Book(int id, String isbn, String title) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+    public void checkedOut(String name) {
+
+    }
+    public void checkIn() {
+
+    }
 }
