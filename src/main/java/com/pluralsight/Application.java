03785765482ead179a32563c330b29ddc4781503
YairVera <yairvera44@gmail.com>
2025-10-07 08:56:57 -0500

    create application class and initialize an empty array of 20 books

---
+++ src/main/java/com/pluralsight/Application.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Book[] books = new Book[20];
+
+        for(Book book: books) {
+            System.out.println(book);
+        }
+
+
+    }
+}
